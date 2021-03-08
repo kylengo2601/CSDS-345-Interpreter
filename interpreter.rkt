ccc249e7f6dcf08adc0e145fc3d675a732bb556a
kris-tran <tungtran2222@gmail.com>
2021-03-08 20:10:42 +0700

    M-state finished

---
+++ interpreter.rkt
@@ -0,0 +1,166 @@
+#lang racket
+(require "simpleParser.rkt")
+
+; Group 4: Aranya Kumar, Kris Tran, Kyle Ngo
+
+; M-integer
+(define M-integer
+  (lambda (expression state)
+    (cond
+      ((number? expression) expression)
+      ((name? expression) (lookupbinding expression state)) 
+      ((eq? (operator expression) '+) (+ (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state)))
+      ((eq? (operator expression) '-) (if (null? (cddr expression)) (- 0 (M-integer (leftoperand expression) state))
+                                (- (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state))))
+      ((eq? (operator expression) '/) (quotient (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state)))
+      ((eq? (operator expression) '%) (remainder (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state)))
+      ((eq? (operator expression) '*) (* (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state)))
+      (else (error 'bad-operator)))))
+
+; M-boolean
+(define M-boolean
+  (lambda (expression state)
+    (cond
+      ((eq? (operator expression) '#t) #t)
+      ((eq? (operator expression) '#f) #f)
+      ((eq? (operator expression) '<) (< (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state)))
+      ((eq? (operator expression) '<=) (or (< (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state)) (eq? (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state))))
+      ((eq? (operator expression) '>) (> (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state)))
+      ((eq? (operator expression) '>=) (or (> (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state)) (eq? (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state))))
+      ((eq? (operator expression) '==) (eq? (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state)))
+      ((eq? (operator expression) '!=) (not (eq? (M-integer (leftoperand expression) state) (M-integer (rightoperand expression) state))))
+      ((eq? (operator expression) '||) (or (M-boolean (leftoperand expression) state) (M-boolean (rightoperand expression) state)))
+      ((eq? (operator expression) '&&) (and (M-boolean (leftoperand expression) state) (M-boolean (rightoperand expression) state)))
+      ((eq? (operator expression) '!) (not (M-boolean (leftoperand expression) state)))
+      (else (error 'bad-operator)))))
+
+; Operators & Operands Abstractions
+(define operator (lambda (expression) (car expression)))
+(define leftoperand cadr)
+(define rightoperand (lambda (expression) (caddr expression)))
+
+(define addbinding
+  (lambda (name value state) (list (add-to-end name (car state)) (add-to-end value (cadr state)))))
+
+(define removebinding
+  (lambda (name state)
+    (cond
+      ((null? state) state)
+      (else (list (removefirst name (car state)) (remove-element-by-index (index name (car state)) (cadr state)))))))
+ 
+(define lookupbinding
+  (lambda (name state) (element-at-index (index name (car state)) (cadr state))))
+      
+; M-state-assignment
+(define M-state-assignment
+  (lambda (expression state)
+    (addbinding (variable expression) (M-integer (expression-value expression) state) (removebinding (variable expression) state))))
+
+; M-state-declaration
+(define M-state-declaration
+  (lambda (expression state)
+    (cond
+      ((not (null? (cddr expression))) (addbinding (variable expression) (M-integer (expression-value expression) state) state))
+      (else (addbinding (variable expression) '() state)))))
+
+; Abstractions for M-state-assignment and declaration
+(define variable cadr)
+(define expression-value (lambda (expression) (caddr expression)))
+
+; M-state-while
+(define M-state-while
+  (lambda (whilestate state)
+    (cond
+      ((M-boolean (condition whilestate) state)
+       (M-state-while whilestate (M-state (body whilestate) state)))
+      (else state))))
+
+; Abstraction for M-state-while
+(define condition cadr)
+(define body caddr)
+
+; M-state-return
+(define M-state-return
+  (lambda (returnstate state)
+    (cond
+      ((pair? (cadr returnstate))
+       (if (isBool (cadr returnstate))
+           (M-boolean (cadr returnstate) state)
+           (M-integer (cadr returnstate) state)))
+      ((eq? (cadr returnstate) '#t) #t)
+      ((eq? (cadr returnstate) '#f) #f)
+      (else (M-boolean (cadr returnstate) state)))))
+
+; M-state-if
+(define M-state-if
+  (lambda (ifstate state)
+    (cond
+      ((M-boolean (ifcondition ifstate) state)
+       (M-state (ifbody ifstate) state))
+      ((not (null? (cdddr ifstate))) (M-state (nextif ifstate) state))
+      (else state))))
+
+; Abstraction for M-state-if
+(define ifcondition (lambda (ifstate) (cadr ifstate)))
+(define ifbody (lambda (ifstate) (caddr ifstate)))
+(define nextif (lambda (ifstate) (cadddr ifstate)))
+
+; M-state
+(define M-state
+  (lambda (expression state)
+    (cond
+      ((eq? (car expression) 'if) (M-state-if expression state))
+      ((eq? (car expression) 'while) (M-state-while expression state))
+      ((eq? (car expression) 'var) (M-state-declaration expression state))
+      ((eq? (car expression) '=) (M-state-assignment expression state))
+      ((eq? (car expression) 'return) (M-state-return expression state))
+      (else (error 'bad-operator)))))
+
+; list of helper methods
+
+; return true if x is a name of a variable (not a list or a number)
+(define name?
+  (lambda (x)
+    (and (not (list? x)) (not (number? x)))))
+; add an element to the end of the list
+(define add-to-end
+  (lambda (a lis)
+    (cond
+      ((null? lis) (cons a lis))
+      (else (cons (car lis) (add-to-end a (cdr lis)))))))
+; remove the first occurence of an element from the list
+(define removefirst
+  (lambda (a lis)
+    (cond
+      ((null? lis) lis)
+      ((eq? a (car lis)) (cdr lis))
+      (else (cons (car lis) (removefirst a (cdr lis)))))))
+; return the index of an element in the list
+(define index
+  (lambda (a lis)
+    (if
+     (equal? (car lis) a) 0 (+ 1 (index a (cdr lis))))))
+; remove an element at a given index
+(define remove-element-by-index
+  (lambda (a lis)
+    (cond
+      ((null? lis) lis)
+      ((= a 0) (cdr lis))
+      (else (cons (car lis) (remove-element-by-index (- a 1) (cdr lis)))))))
+; return an element at a given index
+(define element-at-index
+    (lambda (a lis)
+      (if (= a 0)
+          (car lis)
+          (element-at-index (- a 1) (cdr lis)))))
+; return if an expression is a boolean expression
+(define isBool
+  (lambda (expression)
+    (cond
+      ((or (eq? (car expression) '||) (eq? (car expression) '&&)) #t)
+      ((or (eq? (car expression) '<) (eq? (car expression) '<=)) #t)
+      ((or (eq? (car expression) '>) (eq? (car expression) '>=)) #t)
+      ((or (eq? (car expression) '==) (eq? (car expression) '!=)) #t)
+      ((eq? (car expression) '!) #t)
+      (else #f))))
+      
